e8b5944e136524714ddba0bccfc6288e9a9477ab
xandeq <xandeq@gmail.com>
2021-04-21 13:02:17 -0300

    Forms, useContext, useEffect, Context, Service, Provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,30 @@
-import React from "react";
-import Navbar from "./Navbar";
-import TodoList from "./TodoList";
+import React from 'react';
+import Navbar from './Navbar';
+import TodoList from './TodoList';
+import TodoContext from '../contexts/TodoContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AddTodo from './AddTodo';
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <h1>teste</h1>
-      <TodoList />
-    </div>
-  );
-};
+    return (
+        <TodoContext>
+            <Router>
+                <Navbar></Navbar>
+                <br />
+                <div className="uk-container">
+                    <Switch>
+                        <Route path="/create">
+                            <AddTodo></AddTodo>
+                        </Route>
+                        <Route path="/">
+                            <h4>Minha lista de tarefas</h4>
+                            <TodoList></TodoList>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </TodoContext>
+    );
+}
 
 export default App;
--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useEffect } from "react";
 import { TodoContextType } from "./TodoContextType";
 import { Todo } from "../models/Todo";
+import { get, save } from "../services/TodoService";
 
 export const TodoContext = createContext<TodoContextType>({
   todos: [],
@@ -10,19 +11,27 @@ export const TodoContext = createContext<TodoContextType>({
 });
 
 const TodoProvider = (props: any) => {
-  const [todos, setTodos] = useState<Todo[]>([
-    { id: 1, title: "Ir ao supermercado", done: true },
-    { id: 2, title: "Ir a academia", done: false }
-  ]);
+  const [todos, setTodos] = useState<Todo[]>(get);
+
+  useEffect(() => {
+    save(todos);
+  }, [todos]);
 
   const addTodo = (title: string) => {
-    console.log("Adicionou " + title);
+    const todo: Todo = { id: todos.length, title: title, done: false };
+    setTodos([...todos, todo]);
+    save(todos);
   };
   const removeTodo = (todo: Todo) => {
-    console.log("Removeu" + todo);
+    const index = todos.indexOf(todo);
+    setTodos(todos.filter((_, i) => i !== index));
+    save(todos);
   };
   const toggle = (todo: Todo) => {
-    console.log("TOGGLE ON OFF" + todo);
+    const index = todos.indexOf(todo);
+    todos[index].done = !todo.done;
+    setTodos([...todos]);
+    save(todos);
   };
   return (
     <TodoContext.Provider value={{ todos, addTodo, removeTodo, toggle }}>
